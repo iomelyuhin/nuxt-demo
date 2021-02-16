1d00b0cef331ae22a31af1cd5f81c6cc61aa1c80
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2021-02-16 13:28:42 +0300

    fix nuxt config bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: "static",
-	base: '/nuxt-demo/'
+  base: "/nuxt-demo/",
   head: {
     title: "hello",
     htmlAttrs: {
